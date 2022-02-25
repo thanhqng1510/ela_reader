a6775315b870ad45e256c960e00faa92d013f0ba
thanhqng1510 <thanhqng1510@gmail.com>
2022-02-25 11:07:35 +0700

    Create project.

---
+++ Project/app/src/main/java/com/thanhqng1510/bookreadingapp_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.thanhqng1510.bookreadingapp_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
