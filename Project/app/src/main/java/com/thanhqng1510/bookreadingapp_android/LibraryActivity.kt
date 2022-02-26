12b6141bc85c32c40b9fed81434b8efeebd1bba0
thanhqng1510 <thanhqng1510@gmail.com>
2022-02-26 23:05:03 +0700

    Create layout for LibraryActivity.

--- Project/app/src/main/java/com/thanhqng1510/bookreadingapp_android/LibraryActivity.kt
+++ Project/app/src/main/java/com/thanhqng1510/bookreadingapp_android/LibraryActivity.kt
@@ -1,11 +1,14 @@
 package com.thanhqng1510.bookreadingapp_android
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 
-class MainActivity : AppCompatActivity() {
+class LibraryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_library)
+        supportActionBar?.hide()
     }
 }
