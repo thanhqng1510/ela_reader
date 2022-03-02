eee54a94b8f068829ff033307cdd2606324ede38
thanhqng1510 <thanhqng1510@gmail.com>
2022-03-02 13:21:43 +0700

    Add setupCallbacks function

--- Project/app/src/main/java/com/thanhqng1510/bookreadingapp_android/activities/LibraryActivity.kt
+++ Project/app/src/main/java/com/thanhqng1510/bookreadingapp_android/activities/LibraryActivity.kt
@@ -1,24 +1,32 @@
 package com.thanhqng1510.bookreadingapp_android.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.thanhqng1510.bookreadingapp_android.R
-import com.thanhqng1510.bookreadingapp_android.mocks.MockBooks
 
 class LibraryActivity : AppCompatActivity() {
     private lateinit var bookList: ListView
+    private lateinit var searchBar: SearchView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_library)
-        supportActionBar?.hide()
 
         setupBindings()
+        setupCallbacks()
     }
 
     private fun setupBindings() {
         bookList = findViewById(R.id.book_list)
+        searchBar = findViewById(R.id.search_bar)
         // bookList.adapter = BookListAdapter(this, android.R.layout.book_list_row_layout, MockBooks.getBooks())
+    }
+
+    private fun setupCallbacks() {
+
     }
 }
