1c0610568cc04ad5eb91ed1fe08d1ee81ecf2f56
thanhqng1510 <thanhqng1510@gmail.com>
2022-03-28 16:12:25 +0700

    Replace ListView with RecyclerView.

--- Project/app/src/main/java/com/thanhqng1510/bookreadingapp_android/BookListAdapter.kt
+++ Project/app/src/main/java/com/thanhqng1510/bookreadingapp_android/BookListAdapter.kt
@@ -4,28 +4,39 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.thanhqng1510.bookreadingapp_android.models.Book
 
-class BookListAdapter(private val context: Context, private val rowLayoutId: Int, private val bookList: List<Book>) : BaseAdapter() {
-    override fun getCount(): Int = bookList.size
-
-    override fun getItem(p0: Int): Any = bookList[p0]
-
-    override fun getItemId(p0: Int): Long = p0.toLong()
-
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val view = convertView ?: LayoutInflater.from(parent?.context ?: context).inflate(R.layout.book_list_row_layout, parent, false)
-        val title = view.findViewById<TextView>(R.id.title)
-        val author = view.findViewById<TextView>(R.id.author)
-        val cover = view.findViewById<ImageView>(R.id.cover)
-
-        title.text = bookList[position].title
-        author.text = bookList[position].authors.joinToString(", ")
-        cover.setImageResource(bookList[position].coverId ?: R.mipmap.book_cover_default)
-
-        return view
+class BookListAdapter(private val context: Context, private val bookList: List<Book>) : RecyclerView.Adapter<BookListAdapter.ViewHolder>() {
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val title: TextView = view.findViewById(R.id.title)
+        val author: TextView = view.findViewById(R.id.author)
+        val cover: ImageView = view.findViewById(R.id.cover)
+        val status: ImageView = view.findViewById(R.id.book_status)
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context ?: context).inflate(R.layout.book_list_row_layout, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.title.text = bookList[position].title
+        holder.author.text = bookList[position].authors.joinToString(", ")
+        holder.cover.setImageResource(bookList[position].coverId ?: R.mipmap.book_cover_default)
+
+        if (bookList[position].status != Book.STATUS.FINISHED) {
+            holder.status.setImageResource(
+                when (bookList[position].status) {
+                    Book.STATUS.NEW -> R.drawable.new_status_light
+                    Book.STATUS.READING -> R.drawable.reading_status_light
+                    else -> 0
+                }
+            )
+        }
+    }
+
+    override fun getItemCount(): Int = bookList.size
 }
--- Project/app/src/main/java/com/thanhqng1510/bookreadingapp_android/activities/HomeActivity.kt
+++ Project/app/src/main/java/com/thanhqng1510/bookreadingapp_android/activities/HomeActivity.kt
@@ -1,15 +1,18 @@
 package com.thanhqng1510.bookreadingapp_android.activities
 
 import android.os.Bundle
-import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
+import androidx.recyclerview.widget.RecyclerView
 import com.thanhqng1510.bookreadingapp_android.BookListAdapter
 import com.thanhqng1510.bookreadingapp_android.R
 import com.thanhqng1510.bookreadingapp_android.mocks.MockBooks
+import android.R
+import android.view.View
+
 
 class HomeActivity : AppCompatActivity() {
-    private lateinit var bookList: ListView
+    private lateinit var bookList: RecyclerView
     private lateinit var searchBar: SearchView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,7 +26,7 @@ class HomeActivity : AppCompatActivity() {
     private fun setupBindings() {
         bookList = findViewById(R.id.book_list)
         searchBar = findViewById(R.id.search_bar)
-        bookList.adapter = BookListAdapter(this, R.layout.book_list_row_layout, MockBooks.getBooks())
+        bookList.adapter = BookListAdapter(this, MockBooks.getBooks())
     }
 
     private fun setupCallbacks() {
