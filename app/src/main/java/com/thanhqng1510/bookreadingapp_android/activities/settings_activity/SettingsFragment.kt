374f51a50dbd6d4f9d6c3b6654606f5bd724c18b
thanhqng1510 <thanhqng1510@gmail.com>
2022-05-02 11:38:58 +0700

    Settings UI.

--- app/src/main/java/com/thanhqng1510/bookreadingapp_android/activities/settings_activity/SettingsFragment.kt
+++ app/src/main/java/com/thanhqng1510/bookreadingapp_android/activities/settings_activity/SettingsFragment.kt
@@ -1,10 +1,18 @@
 package com.thanhqng1510.bookreadingapp_android.activities.settings_activity
 
 import android.os.Bundle
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.thanhqng1510.bookreadingapp_android.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
-    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) =
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings_layout, rootKey)
+
+        val alarm: Preference? = findPreference("alarm_setter")
+        alarm?.setOnPreferenceClickListener {
+            TimePickerFragment().show(parentFragmentManager, "timePicker")
+            true
+        }
+    }
 }
---
+++ app/src/main/java/com/thanhqng1510/bookreadingapp_android/activities/settings_activity/TimePickerFragment.kt
@@ -0,0 +1,30 @@
+package com.thanhqng1510.bookreadingapp_android.activities.settings_activity
+
+import android.app.Dialog
+import android.app.TimePickerDialog
+import android.icu.util.Calendar
+import android.os.Bundle
+import android.text.format.DateFormat
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TimePicker
+import androidx.fragment.app.DialogFragment
+import com.thanhqng1510.bookreadingapp_android.databinding.FragmentLibraryBinding
+
+class TimePickerFragment : DialogFragment(), TimePickerDialog.OnTimeSetListener {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        // Use the current time as the default values for the picker
+        val c = Calendar.getInstance()
+        val hour = c.get(Calendar.HOUR_OF_DAY)
+        val minute = c.get(Calendar.MINUTE)
+
+        // Create a new instance of TimePickerDialog and return it
+        return TimePickerDialog(activity, this, hour, minute, DateFormat.is24HourFormat(activity))
+    }
+
+    override fun onTimeSet(view: TimePicker, hourOfDay: Int, minute: Int) {
+        // Do something with the time chosen by the user
+    }
+}
