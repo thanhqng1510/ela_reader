ac4b3abbc8ce56ebb6b9fd822ab8bb1224dfdbd8
thanhqng1510 <thanhqng1510@gmail.com>
2022-05-04 10:03:27 +0700

    Add TODO.

--- app/src/main/java/com/thanhqng1510/ela_reader/application/MainApplication.kt
+++ app/src/main/java/com/thanhqng1510/ela_reader/application/MainApplication.kt
@@ -1,5 +1,7 @@
 package com.thanhqng1510.ela_reader.application
 
+// TODO: Fix screen title says 'Bookmarks' instead of 'Library' after adding a book.
+
 import android.app.Application
 import com.thanhqng1510.ela_reader.utils.constant_utils.ConstantUtils
 import dagger.hilt.android.HiltAndroidApp
